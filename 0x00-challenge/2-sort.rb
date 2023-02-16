5d970dd2cdd9e1f0fe33e6a1414e41ecba54711e
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-02-16 21:48:20 +0200

    sort integers in ascending order

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -4,8 +4,10 @@
 #
 ###
 
+# create an empty array to hold the sorted integers
 result = []
 ARGV.each do |arg|
+    # check if the argument is an integer using a regular expression
     # skip if not integer
     next if arg !~ /^-?[0-9]+$/
 
@@ -16,16 +18,18 @@ ARGV.each do |arg|
     is_inserted = false
     i = 0
     l = result.size
-    while !is_inserted && i < l do
+    while !is_inserted && i < l do # loop through the array until the integer is inserted
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
     end
+    # if the integer has not been inserted, add it to the end of the array
     result << i_arg if !is_inserted
 end
 
+# print the sorted array to the console
 puts result
